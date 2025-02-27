c5671e2b6a25120a70ebe6b51fdd07de19878a35
Rakesh Hadne Sreenath <hadnesreenath@bio.mx>
2025-02-27 18:55:39 +0100

    pylint update

--- vpeleaderboard/data/__init__.py
+++ vpeleaderboard/data/__init__.py
@@ -1,9 +1,5 @@
 """
 Data module containing code and utilities.
 """
-from . import src
-from . import utils
-from . import models
-from . import empty_models
 
-__all__ = ["src", "utils", "models","empty_models"]
+__all__ = ["src", "utils", "models", "empty_models"]
